dc603425e7927b288666cab4501c1cbf73b6185c
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-10-08 15:30:21 -0500

    Add static interface

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact name="John Doe" email="johndoe@gmail.com" phone="597-685-9768" />
+          <Contact name="Jane Doe" email="janedoe@gmail.com" phone="456-546-2345" />
+        </div>
       </div>
     );
   }
